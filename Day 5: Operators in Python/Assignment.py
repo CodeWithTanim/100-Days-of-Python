7ea2fd58699c56a69c0354a11db14e22d67dd880
MD SAMIUR RAHMAN TANIM <86911763+CodeWithTanim@users.noreply.github.com>
2025-02-11 13:19:29 +0530

    Day 5: Operators in Python

---
+++ Day 5: Operators in Python/Assignment.py
@@ -0,0 +1,42 @@
+# x = 10
+
+# print("Initial Value of x:", x)
+
+# x += 5
+# print("After x += 5: ", x)
+
+# x -= 3  # x = x - 3
+# print("After x -= 3:", x)  # Output: 12
+
+# # Multiplication Assignment
+# x *= 2  # x = x * 2
+# print("After x *= 2:", x)  # Output: 24
+
+# # Division Assignment
+# x /= 4  # x = x / 4
+# print("After x /= 4:", x)  # Output: 6.0
+
+# # Modulus Assignment
+# x %= 5  # x = x % 5
+# print("After x %= 5:", x)  # Output: 1.0
+
+# # Exponentiation Assignment
+# x **= 3  # x = x ** 3
+# print("After x **= 3:", x)  # Output: 1.0
+
+# # Floor Division Assignment
+# x //= 1  # x = x // 1
+# print("After x //= 1:", x)  # Output: 1.0
+
+
+Math_Marks = 85
+English_Marks = 90
+Science_Marks = 80
+
+total_Marks = 0
+
+total_Marks += Math_Marks
+total_Marks += English_Marks
+total_Marks += Science_Marks
+
+print("Total Marks: ", total_Marks)
---
+++ Day 5: Operators in Python/allinone.py
@@ -0,0 +1,12 @@
+num1 = 15
+num2 = 7
+
+sum_result = num1 + num2
+print("Sum:", sum_result)
+
+is_Greater = num1 > num2
+
+print("IS num1 greater that num2?", is_Greater)
+
+check = (num1 > 10) and (num2 < 10)
+print("Both conditions True?", check)
