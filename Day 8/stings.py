4ed9d352061e458451a866b608e8b8d445edb0bd
MD SAMIUR RAHMAN TANIM <86911763+CodeWithTanim@users.noreply.github.com>
2025-02-14 12:15:08 +0530

    Day 8: Strings in Python

---
+++ Day 8/stings.py
@@ -0,0 +1,38 @@
+name = "tanim"
+# greeting = "Good Morning"
+greeting = 'Good Morning'
+print(name, greeting)
+
+# poem = """বিদ্রোহী - কাজী নজরুল ইসলাম
+# বল বীর, বল উন্নত মম শির!
+# শির নেহারি' আমারি, নত শির ওই শিখর হিমাদ্রির!
+# বল বীর, আমি চির উন্নত শির,
+# চির উন্নত মম শির, নত শির নহি কারো।"""
+
+# print(poem)
+
+word = "python"
+
+print(word[3])
+print(word[4])
+print(word[0])
+
+# word[0] = 'j'
+
+first_Name = "Samiur"
+last_Name = "Rahman"
+
+full_Name = first_Name + " " + last_Name
+
+print(full_Name)
+
+lough = "Ha"
+
+print(lough * 3)
+
+word2 = "Hello World"
+
+print(len(word2))
+
+print("Hello\nWorld")
+print("Hello\tWorld")
