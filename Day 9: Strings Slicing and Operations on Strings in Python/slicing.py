9a6eb1b3ae91b092839223af776851f5f12aa049
MD SAMIUR RAHMAN TANIM <86911763+CodeWithTanim@users.noreply.github.com>
2025-02-15 19:45:09 +0530

    Day 9: Strings Slicing and Operations on Strings in Python

---
+++ Day 9: Strings Slicing and Operations on Strings in Python/slicing.py
@@ -0,0 +1,20 @@
+text = "Python Programming"
+
+print(text[0:6])
+
+print(text[-11:-1])
+
+print(text[0:6:2])
+
+text1 = "Hello"
+text2 = "World"
+
+print(text1 + text2)
+print(text1 * 4)
+
+print("g" in text)
+print("l" in text)
+
+sentence = "I love Python programming"
+show = sentence[0:6]
+print(show)
