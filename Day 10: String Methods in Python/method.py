f5639e21c25ecefcc4194595dee6d2f312a8242f
MD SAMIUR RAHMAN TANIM <86911763+CodeWithTanim@users.noreply.github.com>
2025-02-16 08:54:54 +0530

    Day 10: String Methods in Python

---
+++ Day 10: String Methods in Python/method.py
@@ -0,0 +1,35 @@
+text = "Code With Tanim"
+
+print(len(text))
+
+text2 = "hello world"
+
+print(text2.upper())
+print(text2.lower())
+
+text3 = "      Python is fun!           "
+
+print(text3.strip())
+
+
+text4 = "I Love Java"
+
+nex_text4 = text4.replace("Java", "Python")
+
+print(nex_text4)
+
+text5 = "Python, Java, C++"
+print(text5.split(","))
+
+url = "https://www.github.com"
+print(url.startswith("https"))
+print(url.endswith("https"))
+
+text6 = "Hello World"
+
+print(text6.find("World"))
+print(text6.find("Tanim"))
+
+text7 = "Python is awesome, Python is easy"
+
+print(text7.count("Python"))
