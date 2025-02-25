d7c7e6ad9e18bcd2aa8fad45f9c6284d93dd558a
MD SAMIUR RAHMAN TANIM <86911763+CodeWithTanim@users.noreply.github.com>
2025-02-25 19:09:32 +0530

    Day 18: Functions in Python

---
+++ Day 18: Functions in Python/easy.py
@@ -0,0 +1,6 @@
+def add_number(a,b):
+    result = a + b
+    return result
+
+sum_result = add_number(5, 10)
+print("The sum is ", sum_result)
---
+++ Day 18: Functions in Python/keywordS.py
@@ -0,0 +1,9 @@
+def person_info(name, age, city):
+    print("Name", name)
+    print("Age", age)
+    print("City", city)
+
+person_info(name="Rahim", age=25, city="Guwahati")
+person_info(name="Sadia", age=23, city="Pabna")
+person_info(name="Karim", age=29, city="Assam")
+person_info(name="Muhit", age=21, city="Dhaka")
